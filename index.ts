b3b2b1e72574866b954c63d4d49526a2fd091301
Matthew T. Gilbride <mgilbride@chariotsolutions.com>
2019-08-06 13:21:23 -0400

    working, basic zoneminder on aws
    zminstall.sh to run zm installation on a ubuntu 16.04 machine
    cdk script to spin up basic aws infra (assuming some existing resources)

---
+++ index.ts
@@ -0,0 +1,107 @@
+import { App, Stack, StackProps } from '@aws-cdk/core';
+import { CfnInstance, Peer, Port, SecurityGroup, Vpc } from "@aws-cdk/aws-ec2";
+import {
+  ApplicationLoadBalancer,
+  ApplicationProtocol,
+  ApplicationTargetGroup,
+  InstanceTarget,
+  SslPolicy,
+  TargetType
+} from "@aws-cdk/aws-elasticloadbalancingv2";
+import { HostedZone, RecordSet, RecordTarget, RecordType } from "@aws-cdk/aws-route53";
+
+
+class ZoneminderStack extends Stack {
+  constructor(scope: App, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const vpc = Vpc.fromLookup(this, "ZM-vpc", { isDefault: true });
+
+    // load balancer security group
+    const loadBalancerSecurityGroup = new SecurityGroup(this, 'ZM-lb-sg', {
+      securityGroupName: 'zoneminder load balancer sg',
+      vpc
+    })
+
+    loadBalancerSecurityGroup.addIngressRule(Peer.ipv4('0.0.0.0/0'), Port.tcp(443))
+    // TODO: forwarding 80 to 443?
+    loadBalancerSecurityGroup.addIngressRule(Peer.ipv4('0.0.0.0/0'), Port.tcp(80))
+    loadBalancerSecurityGroup.addIngressRule(loadBalancerSecurityGroup, Port.allTraffic())
+
+    // ec2 security group
+    const ec2SecurityGroup = new SecurityGroup(this, 'ZM-ec2-sg', {
+      securityGroupName: 'zoneminder ec2 instance sg',
+      vpc,
+    })
+
+    ec2SecurityGroup.addIngressRule(Peer.ipv4(`${process.env.LOCAL_IP}/32` || ''), Port.tcp(22))
+    ec2SecurityGroup.addIngressRule(loadBalancerSecurityGroup, Port.tcp(443))
+    ec2SecurityGroup.addIngressRule(loadBalancerSecurityGroup, Port.tcp(80))
+
+    // ec2 instance
+    const ec2Instance = new CfnInstance(this, 'ZM-ec2-instance', {
+      // todo: build from ubuntu 16.04? via the zminstall.sh script?
+      imageId: process.env.ZONEMINDER_AMI,
+      instanceType: 'm5.large',
+      availabilityZone: 'us-east-1b',
+      // todo: env var
+      keyName: process.env.SSH_KEYNAME,
+      securityGroupIds: [ec2SecurityGroup.securityGroupId],
+      blockDeviceMappings: [{ deviceName: '/dev/sda1', ebs: { deleteOnTermination: false, encrypted: false, volumeSize: 100 }}]
+    })
+
+    // ALB
+    const loadBalancer = new ApplicationLoadBalancer(this, 'ZM-lb', {
+      vpc,
+      internetFacing: true,
+      securityGroup: loadBalancerSecurityGroup,
+    })
+
+    const targetGroup = new ApplicationTargetGroup(this, 'ZM-lb-tg', {
+      port: 80,
+      protocol: ApplicationProtocol.HTTP,
+      targetType: TargetType.INSTANCE,
+      vpc,
+      targets: [new InstanceTarget(ec2Instance.ref, 80)]
+    })
+
+    const httpListener = loadBalancer.addListener('ZM-http-listener', {
+      port: 80,
+    })
+    httpListener.addTargetGroups('ZM-http-listener-tg', { targetGroups: [targetGroup]})
+
+    const httpsListener = loadBalancer.addListener('ZM-https-listener', {
+      port: 443,
+      sslPolicy: SslPolicy.RECOMMENDED,
+      // TODO: generate the cert or use env var?
+      certificateArns: [process.env.CERTIFICATE_ARN || '']
+    })
+    httpsListener.addTargetGroups('ZM-http-listener-tg', { targetGroups: [targetGroup]})
+
+    // Route 53
+    new RecordSet(this, 'ZM-rs', {
+      // TODO: create hosted zone or use env var?
+      zone: HostedZone.fromHostedZoneAttributes(this, 'ZM-hz', {
+        hostedZoneId: process.env.HOSTED_ZONE_ID || '',
+        zoneName: process.env.HOSTED_ZONE_NAME || ''
+      }),
+      // TODO: env var?
+      recordName: process.env.RECORD_NAME,
+      recordType: RecordType.CNAME,
+      target: RecordTarget.fromValues(loadBalancer.loadBalancerDnsName)
+    })
+
+  }
+}
+
+const app = new App()
+new ZoneminderStack(app, process.env.STACK_NAME || '', {
+  env: {
+    region: process.env.AWS_DEFAULT_REGION,
+    account: process.env.AWS_ACCOUNT_NUMBER,
+  },
+  stackName: process.env.STACK_NAME
+});
+
+
+
---
+++ zminstall.sh
@@ -0,0 +1,50 @@
+
+export DEBIAN_FRONTEND=noninteractive
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+sudo echo "mysql-server-5.7 mysql-server/root_password password root" | sudo debconf-set-selections
+sudo echo "mysql-server-5.7 mysql-server/root_password_again password root" | sudo debconf-set-selections
+
+sudo apt-get install lamp-server^ -y
+
+sudo add-apt-repository ppa:iconnor/zoneminder -y
+sudo add-apt-repository ppa:iconnor/zoneminder-1.32 -y
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+
+sudo rm /etc/mysql/my.cnf
+sudo cp /etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/my.cnf
+
+# didn't work
+# nano /etc/mysql/my.cnf
+# ctrl+w [msqld]
+# ctrl+o. ctrl+x
+sudo sed -i '/\[mysqld\]/a sql_mode = NO_ENGINE_SUBSTITUTION' /etc/mysql/my.cnf
+
+sudo systemctl restart mysql
+
+sudo apt-get install zoneminder -y
+
+sudo chmod 740 /etc/zm/zm.conf
+sudo chown root:www-data /etc/zm/zm.conf
+sudo chown -R www-data:www-data /usr/share/zoneminder/
+
+sudo a2enmod cgi
+sudo a2enmod rewrite
+sudo a2enconf zoneminder
+
+sudo a2enmod expires
+sudo a2enmod headers
+
+sudo systemctl enable zoneminder
+sudo systemctl start zoneminder
+
+sudo sed -i '/\[Date\]/a date.timezone = America/New_York' /etc/php/7.0/apache2/php.ini
+
+sudo systemctl reload apache2
+
+# in zoneminder ui config set AUTH_RELAY to none
