0014012ac3b4c1687a500ce801bdbc204c8eb461
Matthew T. Gilbride <mgilbride@chariotsolutions.com>
2020-09-08 10:06:14 -0400

    initial conversion to upload raw files from zm to s3

--- python/src/zm-s3-upload.py
+++ python/src/zm-s3-upload.py
@@ -5,13 +5,10 @@ import sys
 import traceback
 import logging
 
+objects = ['person', 'bicycle', 'car', 'motorbike', 'bus', 'truck', 'dog', 'cat']
 
-def generate_s3_key(monitor, date_time, description):
-    split = date_time.split()
-    date = split[0]
-    time = split[1]
-    hours, minutes, seconds = time.split(':')
-    objects = ['person', 'bicycle', 'car', 'motorbike', 'bus', 'truck', 'dog', 'cat']
+
+def generate_s3_suffix(description):
     suffix = ''
     if len(description) > 0:
         if description.find('Motion') != -1:
@@ -19,7 +16,21 @@ def generate_s3_key(monitor, date_time, description):
         for object in objects:
             if description.find(object) != -1:
                 suffix += f"-{object}"
-    return f"{date}/{hours}:{minutes}_{monitor}{suffix}.avi"
+    return suffix
+
+
+def generate_s3_path(monitor, date_time):
+    split = date_time.split()
+    date = split[0]
+    time = split[1]
+    hours, minutes, seconds = time.split(':')
+    return f"{date}/{hours}:{minutes}_{monitor}"
+
+
+def generate_s3_key(monitor, date_time, description):
+    prefix = generate_s3_path(monitor, date_time)
+    suffix = generate_s3_suffix(description)
+    return f"{prefix}{suffix}.avi"
 
 
 # unused for now - could possibly use ffmpeg in a lambda instead of forcing ZM zerver to do expensive jpg/avi conversion
@@ -41,12 +52,23 @@ def upload_video(path, monitor, date_time, description):
         logging.error(f"did not find avi file in directory: ${path}")
 
 
+def upload_raw_zm_files(path, monitor, date_time, description):
+    s3_client = client('s3')
+    bucket = 'zoneminder.mattgilbride.com'
+    s3_path = f"{generate_s3_path(monitor, date_time)}{generate_s3_suffix(description)}"
+    for f in listdir(path):
+        file_name = f"{path}/{f}"
+        object_name = f"{s3_path}/{f}"
+        s3_client.upload_file(file_name, bucket, object_name)
+    s3_client.put_object(description.encode(), bucket, f"{s3_path}/-description.txt")
+
+
 def main(date_time, duration, frames, monitor, description, path):
     logging.basicConfig(filename='/var/log/zm/s3-upload.log', level=logging.INFO)
     logging.info(f"Starting S3 Upload: {path}")
     try:
         # create_video(path, frames, duration)
-        upload_video(path, monitor, date_time, description)
+        upload_raw_zm_files(path, monitor, date_time, description)
         logging.info(f"Finished S3 Upload: {path}")
     except Exception as e:
         track = traceback.format_exc()
