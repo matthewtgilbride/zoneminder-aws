b4dc765bdc488e8e680c75a6f4a4e8c64492ed8e
Matthew T. Gilbride <mgilbride@chariotsolutions.com>
2020-09-12 09:53:39 -0400

    get the scripts written down for letsencrypt

--- install/nodeawsinstall.sh
+++ install/nodeawsinstall.sh
@@ -1,3 +1,4 @@
 apt-get update
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
-cd usr/bin && npm install aws-sdk
+apt-get install -y nodejs
+cd /usr/bin && npm install aws-sdk
--- install/sslcertinstall.sh
+++ install/sslcertinstall.sh
@@ -5,6 +5,7 @@ apt-get update
 apt-get install certbot -y
 apt-get install python3-certbot-dns-route53 -y
 apt-get install certbot python-certbot-apache -y
+chown -R www-data:root /etc/letsencrypt/
 certbot certonly -d zoneminder.mattgilbride.com --non-interactive --agree-tos --email matthewtgilbride@@gmail.com --dns-route53
 certbot -d zoneminder.mattgilbride.com --non-interactive --agree-tos --apache --redirect
 
